0a5daf398be0485fbb395ce007ae38e2ceff5bdc
MohammadHarun <mohammadharun5678@gmail.com>
2020-12-12 23:31:11 +0600

    First commit

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform target;
+
+    private void Update()
+    {
+        if(target.position.y > transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] blocks;
+    public float minTime;
+    public float maxTime;
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnEnemy", Random.Range(minTime, maxTime), Random.Range(minTime, maxTime));
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(blocks[Random.Range(0, 3)], transform.position, transform.rotation);
+    }
+}
